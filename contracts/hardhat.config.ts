7f42f28d7c0b891896e5a97240d6aa267531829e
0xmad <0xmad@users.noreply.github.com>
2024-01-10 13:16:53 -0600

    chore(contracts): update @openzeppelin/contracts, solidity compiler version and compatibility fixes

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -8,7 +8,7 @@ import type { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.10",
+    version: "0.8.20",
     settings: {
       optimizer: {
         enabled: true,
